f23f4194b0c6176299dedf68422ccaee540fa756
Ramsay Lanier <ramsaylanier@gmail.com>
2018-06-19 14:23:29 -0400

    remove body-parser dep

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import cors from 'cors'
-import bodyParser from 'body-parser'
 import {ApolloServer} from 'apollo-server'
 import {WordExpressDefinitions, WordExpressResolvers} from 'wordexpress-schema'
 import {connectors} from './db'
